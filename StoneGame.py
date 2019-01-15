936c8f9fe43e9a73ba333358f0c2ea69042295c6
alexandruparaschiv <alexandruparaschiv@Alexandrus-MacBook-Pro.local>
2019-01-15 20:19:27 +0000

    initial commit

---
+++ StoneGame.py
@@ -0,0 +1,24 @@
+class Solution:
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        total_piles = sum(piles)
+        no_piles = len(piles)
+        max_Alex = 0
+        cache = [[ 0 for i in range(no_piles) ] for j in range(no_piles)]
+        def helper(curr_piles,i,j):
+            if cache[i][j] != 0:
+                return cache[i][j]
+            if i==j:
+                return curr_piles[0]
+            else:
+                max_Alex = max(curr_piles[0]+helper(curr_piles[1:],i+1,j),curr_piles[-1]+helper(curr_piles[:-1],i,j-1))
+                cache[i][j] = max_Alex
+                return max_Alex
+        max_Alex = helper(piles,0,len(piles)-1)
+        if max_Alex > total_piles - max_Alex:
+            return True
+        else:
+            return False
